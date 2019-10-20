4963d6e2b91b36d665a1eee62c0c652965dcba54
s1210287 <55116528+s1210287@users.noreply.github.com>
2019-10-19 20:33:35 -0400

    Update InputListandSearch.py

--- InputListandSearch.py
+++ InputListandSearch.py
@@ -3,17 +3,17 @@
     
 names  = []
 for x in range(0,10):
-    aName = input("enter first name:")
+    aName = input("Enter a name to search for:")
     names.append(aName)
 
-end = input('type End to quit: ')
+end = input('Type End to end the program: ')
 if end == 'End':
     exit(0)
 
 print(names)
 endChoice = False
 while not endChoice:
-    search = input("enter search name:")
+    search = input("Enter a search name:")
 
     if search in names:
         print(search, "was found")
